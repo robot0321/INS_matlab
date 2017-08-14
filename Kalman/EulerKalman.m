6c2191f6a6e3b6a17ed07ea1b0fe8cf6894c59d5
jaeyoung chung <robot0321@naver.com>
2017-08-14 16:41:12 +0900

    gyro acc kalman

---
+++ Kalman/EulerKalman.m
@@ -0,0 +1,28 @@
+function result = EulerKalman(gy,q,dt)
+    
+    persistent H Q R x P firstRun
+    z = q;
+    
+    if isempty(firstRun)
+       H = eye(4);
+       Q = 0.0001*eye(4);
+       R = 10*eye(4);
+       x = [1; 0; 0; 0];
+       P = 1*eye(4);
+       
+       firstRun=1;
+    end
+
+    A = eye(4) + 0.5*dt*[0,   -gy(1), -gy(2), -gy(3);
+                         gy(1),  0,   gy(3), -gy(2);
+                         gy(2), -gy(3),  0,  gy(1);
+                         gy(3), gy(2), -gy(1),  0];
+    
+    xp = A*x;
+    Pp = A*P*A' + Q;
+    K = Pp*H'/(H*Pp*H' + R);
+    x = xp + K*(z - H*xp);
+    P = Pp - K*H*Pp;
+    
+    result = x;
+end
--- Untitled4.m
+++ Untitled4.m
@@ -26,14 +26,15 @@ az = az./mag;
 
 angA = zeros(N,2);
 for i=1:N
-    angA(i,:) = EulerAccel(ax(i),az(i),1);
+    angA(i,:) = EulerAccel(az(i),ax(i),1);
 end
 Ca = angle2dcm(zeros(N,1), angA(:,1), angA(:,2));
+qa = angle2quat(zeros(N,1), angA(:,1), angA(:,2))';
 
 qG = zeros(4,N);
 angG = zeros(N,3);
 qG(:,1) = angle2quat(0, angA(1,1), angA(1,2));
-angG(1,:) = [angA(1,2), angA(1,1), 0]; % Y P R
+angG(1,:) = [angA(1,2), angA(1,1), 0]; % R P Y
 for i=1:N-1
     qG(:,i+1) = QuatGyro(qG(:,i), [gx(i), gy(i), gz(i)], (M(i+1,1) - M(i,1))/1000);
     angG(i+1,:) = EulerGyro(angG(i,:), [gx(i), gy(i), gz(i)], (M(i+1,1) - M(i,1))/1000);
@@ -41,5 +42,21 @@ end
 Cg = quat2dcm(qG');
 Cgg = angle2dcm(angG(:,3), angG(:,2), angG(:,1));
 
-attitude_visualize(Cg,Cgg);
+qt = zeros(4,N);
+for i=1:N
+   qt(:,i)=EulerKalman(angG(i,:), qa(:,i), 0.1);
+end
+Ck = quat2dcm(qt');
+[Ky, Kp, Kr] = quat2angle(qt');
+
+figure()
+hold on;
+plot(t(1:490), qa(1:490))
+plot(t(1:490), qG(1:490))
+plot(t(1:490), qt(1:490))
+
+
+%%
+figure(4);
+attitude_visualize(Cg,Ck);
 
